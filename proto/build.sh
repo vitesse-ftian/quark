e3ddc655bc34f0ff82eaa26af4592057920730d6
Feng Tian <fengttt@gmail.com>
2015-09-30 09:30:50 -0700

    Start green quark.

--- proto/build.sh
+++ proto/build.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 
-${TOOLCHAIN_DIR}/installed/bin/protoc -I=. --java_out=../llql/llql_proto/src ns.proto
-${TOOLCHAIN_DIR}/installed/bin/protoc -I=. --java_out=../llql/llql_proto/src data.proto
-${TOOLCHAIN_DIR}/installed/bin/protoc -I=. --java_out=../llql/llql_proto/src err.proto
-${TOOLCHAIN_DIR}/installed/bin/protoc -I=. --java_out=../llql/llql_proto/src expr.proto
-${TOOLCHAIN_DIR}/installed/bin/protoc -I=. --java_out=../llql/llql_proto/src query.proto
-${TOOLCHAIN_DIR}/installed/bin/protoc -I=. --java_out=../llql/llql_proto/src quark.proto
+protoc -I=. --java_out=../llql/llql_proto/src ns.proto
+protoc -I=. --java_out=../llql/llql_proto/src data.proto
+protoc -I=. --java_out=../llql/llql_proto/src err.proto
+protoc -I=. --java_out=../llql/llql_proto/src expr.proto
+protoc -I=. --java_out=../llql/llql_proto/src query.proto
+protoc -I=. --java_out=../llql/llql_proto/src quark.proto
+
+protoc -I=. --cpp_out=../green/llql ns.proto
+protoc -I=. --cpp_out=../green/llql data.proto
+protoc -I=. --cpp_out=../green/llql err.proto
+protoc -I=. --cpp_out=../green/llql expr.proto
+protoc -I=. --cpp_out=../green/llql query.proto
+protoc -I=. --cpp_out=../green/llql quark.proto
+
 
